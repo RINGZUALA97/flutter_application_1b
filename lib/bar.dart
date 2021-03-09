0fa563ab3fb04f94edd59d6c2982f0e565d911a2
RINGZUALA97 <maringtea123@gmail.com>
2021-03-09 00:45:51 -0800

    sd

--- lib/bar.dart
+++ lib/bar.dart
@@ -1,140 +1,151 @@
-import 'package:flutter/material.dart';
+// // import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// // void main() {
+// //   runApp(MyApp());
+// // }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    );
-  }
-}
+// // class MyApp extends StatelessWidget {
+// //   // This widget is the root of your application.
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return MaterialApp(
+// //       title: 'Flutter Demo',
+// //       theme: ThemeData(
+// //         primarySwatch: Colors.blue,
+// //       ),
+// //       home: HomePage(),
+// //     );
+// //   }
+// // }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
+// // class HomePage extends StatefulWidget {
+// //   @override
+// //   _HomePageState createState() => _HomePageState();
+// // }
 
-class _HomePageState extends State<HomePage> {
-  int _currentIndex = 0;
-  final tabs = [
-    Center(
-      child: Text('home'),
-    ),
-    Center(
-      child: Text('camera'),
-    ),
-    Center(
-      child: Text('person'),
-    ),
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('navigation example'),
-      ),
-      body: tabs[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          iconSize: 50,
-          unselectedFontSize: 10,
-          selectedFontSize: 20,
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: ('home'),
-                backgroundColor: Colors.amber),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.camera),
-                label: ('camera'),
-                backgroundColor: Colors.red),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: ('search'),
-                backgroundColor: Colors.green),
-          ],
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          }),
-    );
-  }
-}
-//navigation theme
-import 'package:flutter/material.dart';
+// // class _HomePageState extends State<HomePage> {
+// //   int _currentIndex = 0;
+// //   final tabs = [
+// //     Center(
+// //       child: Text('home'),
+// //     ),
+// //     Center(
+// //       child: Text('camera'),
+// //     ),
+// //     Center(
+// //       child: Text('person'),
+// //     ),
+// //   ];
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       appBar: AppBar(
+// //         title: Text('navigation example'),
+// //       ),
+// //       body: tabs[_currentIndex],
+// //       bottomNavigationBar: BottomNavigationBar(
+// //           currentIndex: _currentIndex,
+// //           iconSize: 50,
+// //           unselectedFontSize: 10,
+// //           selectedFontSize: 20,
+// //           items: [
+// //             BottomNavigationBarItem(
+// //                 icon: Icon(Icons.home),
+// //                 label: ('home'),
+// //                 backgroundColor: Colors.amber),
+// //             BottomNavigationBarItem(
+// //                 icon: Icon(Icons.camera),
+// //                 label: ('camera'),
+// //                 backgroundColor: Colors.red),
+// //             BottomNavigationBarItem(
+// //                 icon: Icon(Icons.search),
+// //                 label: ('search'),
+// //                 backgroundColor: Colors.green),
+// //           ],
+// //           onTap: (index) {
+// //             setState(() {
+// //               _currentIndex = index;
+// //             });
+// //           }),
+// //     );
+// //   }
+// // }
+// // //navigation theme
+// // import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// // void main() {
+// //   runApp(MyApp());
+// // }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        appBar: AppBar(
-          title: Text('Application'),
-          backgroundColor: Colors.red,
+// // class MyApp extends StatelessWidget {
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return MaterialApp(
+// //       home: Scaffold(
+// //         backgroundColor: Colors.teal,
+// //         appBar: AppBar(
+// //           title: Text('Application'),
+// //           backgroundColor: Colors.red,
 
+// //            Text(
+// //                 'LINUX',
+// //               ),
+// //               Container(
+// //                 colors: Colors.white,
+// //                 margin:Edgemargin
+// //                 child: Row(
+// //                   children: <Widget>[
+// //                     Icon(
+// //                       Icons.access_alarms_rounded,
+// //                       size: 30.0,
+// //                     ),
+// //                     Text('Operating system')
+// //                   ],
+// //                 ),
+// //               )
 
-           Text(
-                'LINUX',
-              ),
-              Container(
-                colors: Colors.white,
-                margin:Edgemargin
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.access_alarms_rounded,
-                      size: 30.0,
-                    ),
-                    Text('Operating system')
-                  ],
-                ),
-              ) 
-              
-              
-              
-              
-              ds
-              
-              
-              import 'package:flutter/material.dart';
+// //               ds
 
-void main() {
-  runApp(MyApp());
-}
+// //               import 'package:flutter/material.dart';
+
+// // void main() {
+// //   runApp(MyApp());
+// // }
+
+// // // class MyApp extends StatelessWidget {
+// // //   @override
+// // //   Widget build(BuildContext context) {
+// // //     return MaterialApp(
+// // //       home: Scaffold(
+// // //         backgroundColor: Colors.teal,
+// // //         body: SafeArea(
+// // //           child: Column(
+// // //             children: <Widget>[
+// // //               CircleAvatar(
+// // //                 radius: 50.0,
+// // //                 backgroundColor: Colors.red,
+// // //               ),
+// // //             ],
+// // //           ),
+// // //         ),
+// // //       ),
+// // //     );
+// // //   }
+// // // }
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
 
 // class MyApp extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
 //       home: Scaffold(
-//         backgroundColor: Colors.teal,
-//         body: SafeArea(
-//           child: Column(
-//             children: <Widget>[
-//               CircleAvatar(
-//                 radius: 50.0,
-//                 backgroundColor: Colors.red,
-//               ),
-//             ],
-//           ),
-//         ),
+
 //       ),
 //     );
 //   }
 // }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,360 +12,282 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: FirstPage(),
-    );
-  }
-}
-
-class FirstPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('ROUTING'),
-      ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '1st Page',
-              style: TextStyle(fontSize: 50),
-            ),
-            RaisedButton(
-              child: Text('go next page'),
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => SecondPage(data: 'hello there'),
-                  ),
-                );
-              },
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Container(
-              height: 200.0,
-              width: 200.0,
-              color: Colors.blue,
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Container(
-              height: 200.0,
-              width: 200.0,
-              color: Colors.red,
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        iconSize: 50,
-        unselectedFontSize: 10,
-        selectedFontSize: 20,
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: ('home'),
-              backgroundColor: Colors.amber),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.camera),
-              label: ('camera'),
-              backgroundColor: Colors.red),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: ('search'),
-              backgroundColor: Colors.green),
-        ],
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  final String data;
-  SecondPage({
-    Key key,
-    @required this.data,
-  }) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Previous Page'),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Text(
-              '2nd Page',
-              style: TextStyle(fontSize: 50),
-            ),
-            RaisedButton(
-              child: Text('go next page'),
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => ThirdPage(data: 'hello there'),
-                  ),
-                );
-              },
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            CircleAvatar(
-              radius: (10),
-              backgroundImage: AssetImage('images/tick.jpeg'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ThirdPage extends StatelessWidget {
-  final String data;
-  ThirdPage({
-    Key key,
-    @required this.data,
-  }) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.teal,
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Previous Page'),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Text(
-              '3rd Page',
-              style: TextStyle(fontSize: 50),
-            ),
-            RaisedButton(
-              child: Text('go next page'),
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => LastPage(data: 'final page'),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class LastPage extends StatelessWidget {
-  final String data;
-  LastPage({
-    Key key,
-    @required this.data,
-  }) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Previous Page'),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Text(
-              '4rd Page',
-              style: TextStyle(fontSize: 50),
-            ),
-            Text(
-              'last Page',
-              style: TextStyle(fontSize: 50),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-//login
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.fromLTRB(10.0, 50.0, 0.0, 0.0),
-                  child: Text(
-                    'Hi',
-                    style:
-                        TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(10.0, 20.0, 0.0, 0.0),
-                  child: Text(
-                    'Welcome',
-                    style:
-                        TextStyle(fontSize: 70.0, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 25.0, left: 20.0, right: 20.0),
-                  child: Column(
-                    children: <Widget>[
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.red),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.red),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 30.0,
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Password ',
-                          labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.red),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.red),
-                          ),
-                        ),
-                        obscureText: true,
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      Container(
-                        alignment: Alignment(1.0, 0.0),
-                        padding: EdgeInsets.only(top: 15.0, left: 20.0),
-                        child: InkWell(
-                          child: Text(
-                            'Forget Password?',
-                            style: TextStyle(
-                                color: Colors.green,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Montserrat',
-                                decoration: TextDecoration.underline),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 40.0,
-                      ),
-                      Container(
-                        height: 50.0,
-                        child: Material(
-                          borderRadius: BorderRadius.circular(20.0),
-                          shadowColor: Colors.greenAccent,
-                          color: Colors.green,
-                          elevation: 7.0,
-                          child: GestureDetector(
-                            onTap: () {},
-                            child: Center(
-                              child: Text(
-                                'Login',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Montserrat',
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text('Or'),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        height: 50.0,
-                        color: Colors.transparent,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.black,
-                              style: BorderStyle.solid,
-                              width: 1.0,
-                            ),
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(20.0),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Center(
-                                child: Text(
-                                  'Login with Facebook',
-                                  style: TextStyle(
-                                    color: Colors.teal,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Montserrat',
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Windows',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ), // ),v
+              Container(
+                margin: EdgeInsets.all(30.0),
+                height: 200.0,
+                width: 400.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight, end: Alignment(0.8, 0.0),
+                    colors: [
+                      const Color(0xffee0000),
+                      const Color(0xffeeee00),
                     ],
+
+                    // boxShadow: [
+                    //   BoxShadow(
+                    //     color: Colors.amber,
+                    //     blurRadius: 10.0,
+                    //   ),
+                    // ],
+                    // borderRadius: BorderRadius.circular(30.0),
+                    // color: Colors.teal,
+                    // border: Border.all(
+                    //   width: 5,
+                    //   color: Colors.blue,
+                    //   style: BorderStyle.solid,
+                    // ),
                   ),
                 ),
-              ],
-            ),
-          )
-        ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                margin: EdgeInsets.all(30.0),
+                height: 200.0,
+                width: 400.0,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.green,
+                      blurRadius: 10.0,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(30.0),
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomLeft, end: Alignment(0.8, 0.0),
+                    colors: [
+                      Colors.teal[600],
+                      Colors.blue,
+                    ],
+
+                    // boxShadow: [
+                    //   BoxShadow(
+                    //     color: Colors.amber,
+                    //     blurRadius: 10.0,
+                    //   ),
+                    // ],
+                    // borderRadius: BorderRadius.circular(30.0),
+                    // color: Colors.teal,
+                    // border: Border.all(
+                    //   width: 5,
+                    //   color: Colors.blue,
+                    //   style: BorderStyle.solid,
+                    // ),
+                  ),
+                ),
+              ) // ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+// Container(
+//   margin: EdgeInsets.all(30.0),
+//   height: 200.0,
+//   width: 400.0,
+//   decoration: BoxDecoration(
+//     borderRadius: BorderRadius.only(
+//       bottomRight: Radius.circular(100.0),
+//     ),
+//     color: Colors.teal,
+//     border: Border.all(
+//       width: 5,
+//       color: Colors.blue,
+//       style: BorderStyle.solid,
+//     ),
+//   ),
+// class FirstPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       appBar: AppBar(
+//         centerTitle: true,
+//         title: Text('ROUTING'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               '1st Page',
+//               style: TextStyle(fontSize: 50),
+//             ),
+//             ElevatedButton(
+//               child: Text('go next page'),
+//               onPressed: () {
+//                 Navigator.of(context).push(
+//                   MaterialPageRoute(
+//                     builder: (context) => SecondPage(data: 'hello there'),
+//                   ),
+//                 );
+//               },
+//             ),
+//             SizedBox(
+//               height: 20.0,
+//             ),
+//             Container(
+//               height: 200.0,
+//               width: 200.0,
+//               color: Colors.blue,
+//             ),
+//             SizedBox(
+//               height: 20.0,
+//             ),
+//             Container(
+//               height: 200.0,
+//               width: 200.0,
+//               color: Colors.red,
+//             ),
+//           ],
+//         ),
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         iconSize: 50,
+//         unselectedFontSize: 10,
+//         selectedFontSize: 20,
+//         items: [
+//           BottomNavigationBarItem(
+//               icon: Icon(Icons.home),
+//               label: ('home'),
+//               backgroundColor: Colors.amber),
+//           BottomNavigationBarItem(
+//               icon: Icon(Icons.camera),
+//               label: ('camera'),
+//               backgroundColor: Colors.red),
+//           BottomNavigationBarItem(
+//               icon: Icon(Icons.search),
+//               label: ('search'),
+//               backgroundColor: Colors.green),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class SecondPage extends StatelessWidget {
+//   final String data;
+//   SecondPage({
+//     Key key,
+//     @required this.data,
+//   }) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       appBar: AppBar(
+//         centerTitle: true,
+//         title: Text('Previous Page'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           children: <Widget>[
+//             Text(
+//               '2nd Page',
+//               style: TextStyle(fontSize: 50),
+//             ),
+//             ElevatedButton(
+//               child: Text('go next page'),
+//               onPressed: () {
+//                 Navigator.of(context).push(
+//                   MaterialPageRoute(
+//                     builder: (context) => ThirdPage(data: 'hello there'),
+//                   ),
+//                 );
+//               },
+//             ),
+//             SizedBox(
+//               height: 20.0,
+//             ),
+//             CircleAvatar(
+//               radius: (10),
+//               backgroundImage: AssetImage('images/tick.jpeg'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class ThirdPage extends StatelessWidget {
+//   final String data;
+//   ThirdPage({
+//     Key key,
+//     @required this.data,
+//   }) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.teal,
+//       appBar: AppBar(
+//         centerTitle: true,
+//         title: Text('Previous Page'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           children: <Widget>[
+//             Text(
+//               '3rd Page',
+//               style: TextStyle(fontSize: 50),
+//             ),
+//             ElevatedButton(
+//               child: Text('go next page'),
+//               onPressed: () {
+//                 Navigator.of(context).push(
+//                   MaterialPageRoute(
+//                     builder: (context) => LastPage(data: 'final page'),
+//                   ),
+//                 );
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class LastPage extends StatelessWidget {
+//   final String data;
+//   LastPage({
+//     Key key,
+//     @required this.data,
+//   }) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Previous Page'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           children: <Widget>[
+//             Text(
+//               '4rd Page',
+//               style: TextStyle(fontSize: 50),
+//             ),
+//             Text(
+//               'last Page',
+//               style: TextStyle(fontSize: 50),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
