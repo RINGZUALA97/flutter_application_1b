829c27f8acd220088af03fd8ea3a4b72b43fa077
RINGZUALA97 <80247961+RINGZUALA97@users.noreply.github.com>
2021-03-16 17:19:25 +0530

    Update example.dart

--- lib/example.dart
+++ lib/example.dart
@@ -61,4 +61,88 @@ class ChoicePage extends StatelessWidget {
     );
   }
 }
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ScreenPage(),
+    );
+  }
+}
+
+class ScreenPage extends StatefulWidget {
+  @override
+  _ScreenPageState createState() => _ScreenPageState();
+}
+
+class _ScreenPageState extends State<ScreenPage> {
+  String firstInput;
+  String secondInput;
+  final TextEditingController _firstenter = TextEditingController();
+  final TextEditingController _secondenter = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: TextFormField(
+                  controller: _firstenter,
+                  decoration: InputDecoration(
+                    labelText: 'Enter Name',
+                  ),
+                  onChanged: (value) {
+                    firstInput = value;
+                  },
+                ),
+              ),
+              Container(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text('click'),
+                ),
+              ),
+              Container(
+                child: TextFormField(
+                  controller: _secondenter,
+                  decoration: InputDecoration(
+                    labelText: 'Enter Name Again ',
+                  ),
+                  onChanged: (value) {
+                    secondInput = value;
+                  },
+                ),
+              ),
+              Container(
+                child: TextButton(
+                  onPressed: () {
+                    checking();
+                  },
+                  child: Text('click'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void checking() {
+    if (firstInput == secondInput) {
+      print('SAME');
+    } else {
+      print('NOT ACCESSIBLE');
+    }
+  }
+}
+
  
